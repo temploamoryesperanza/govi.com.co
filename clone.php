b38c428a237dc34f7acfd942d756aaa3acf3104a
una liendrita mas <temploamoryesperanza@gmail.com>
2022-02-05 00:38:29 -0500

    test

---
+++ clone.php
@@ -0,0 +1,164 @@
+9b13c8761f0e2e2b5934258a9a64bcbd667699cf
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Mi Vacuna</title>
+<link rel="icon" href="https://mivacuna.sispro.gov.co/MiVacuna/favicon.ico">
+<link href="https://mivacuna.sispro.gov.co/MiVacuna/Content/css?v=zr9T_w9scPTVEjdN-6HrtKmrz3c0y-ZtlcdI4zAdu3s1" rel="stylesheet"/>
+
+<script src="/MiVacuna/bundles/modernizr?v=inCVuEFe6J4Q07A0AcRsbJic_UE5MwpRMNGcOtk94TE1"></script>
+
+
+
+
+
+
+<link rel="icon" href="/MiVacuna/favicon.ico" />
+</head>
+<body style="background-color:#FFFFFF; padding-top:80px; ">
+<div class="navbar navbar-inverse navbar-fixed-top" style="background-color: #004884; height: 70px;">
+<div class="navbar-inner">
+<div class="navbar-brand navbar-left" style="margin-top:-12px; vertical-align:central;">
+<img src="https://mivacuna.sispro.gov.co/MiVacuna/Images/logoPrincipal.png" alt="Mi vacuna - Ministerio de Salud y Protección Social " height="60" style="vertical-align:central;" />
+
+</div>
+
+</div>
+
+</div>
+<div>
+
+
+
+
+<div class="container body-content">
+<section class="secc">
+<div class="row col-lg-10 col-lg-offset-1">
+
+<div style="background-color: #FAFAFA; padding-top:0px;padding-bottom:10px;">
+
+<img src="https://mivacuna.sispro.gov.co/MiVacuna/Images/MinSaludDigital.png" alt="" style="width:100%; padding-bottom:10px;" />
+
+<div class="col-md-12">
+<h5 class="CertDigTit1">Nombres y apellidos / Full name</h5>
+<h5 class="CertDigVal1">UNA LIENDRITA MAS</h5>
+</div>
+<div class="col-md-12">
+<div class="col-md-6">
+<h5 class="CertDigTit2">Tipo de identificación / ID Type</h5>
+<h5 class="CertDigVal2">CC</h5>
+</div>
+<div class="col-md-6">
+<h5 class="CertDigTit2">Número de identificación / ID Number</h5>
+<h5 class="CertDigVal2">0987123456</h5>
+</div>
+</div>
+
+<div class="col-md-12">
+<h5 class="CertDigTit1">Datos de vacunación / Vaccination detail</h5>
+</div>
+<div>
+<div class="col-md-12">
+<div class="col-md-6">
+<h5 class="CertDigTit2" style="font-weight:bold;">Dosis / Number</h5>
+<h5 class="CertDigVal1">Primera</h5>
+</div>
+<div class="col-md-6">
+<h5 class="CertDigTit2">Lote / Vaccine batch</h5>
+<h5 class="CertDigVal2">NH0102</h5>
+</div>
+</div>
+<div class="col-md-12">
+<div class="col-md-6">
+<h5 class="CertDigTit2">Vacuna / Vaccine</h5>
+<h5 class="CertDigVal2">ASTRAVENECA :v</h5>
+</div>
+<div class="col-md-6">
+<h5 class="CertDigTit2">Centro Vacunador / Administering Centre</h5>
+<h5 class="CertDigVal2">MEDELLIN LA CALLE DEL PECADO</h5>
+</div>
+</div>
+<div class="col-md-12">
+<div class="col-md-6">
+<h5 class="CertDigTit2">Fecha de aplicación / Date of Vaccination</h5>
+<h5 class="CertDigVal2">2021-08-15</h5>
+</div>
+<div class="col-md-6">
+<h5 class="CertDigTit2">País de vacunación / Country of Vaccination</h5>
+<h5 class="CertDigVal2">CO-Colombia</h5>
+</div>
+</div>
+</div>
+<div>
+<div class="col-md-12">
+<div class="col-md-6">
+<h5 class="CertDigTit2" style="font-weight:bold;">Dosis / Number</h5>
+<h5 class="CertDigVal1">Primera</h5>
+</div>
+<div class="col-md-6">
+<h5 class="CertDigTit2">Lote / Vaccine batch</h5>
+<h5 class="CertDigVal2">NH0102</h5>
+</div>
+</div>
+<div class="col-md-12">
+<div class="col-md-6">
+<h5 class="CertDigTit2">Vacuna / Vaccine</h5>
+<h5 class="CertDigVal2">ASTRAVENECA :v</h5>
+</div>
+<div class="col-md-6">
+<h5 class="CertDigTit2">Centro Vacunador / Administering Centre</h5>
+<h5 class="CertDigVal2">MEDELLIN LA CALLE DEL PECADO</h5>
+</div>
+</div>
+<div class="col-md-12">
+<div class="col-md-6">
+<h5 class="CertDigTit2">Fecha de aplicación / Date of Vaccination</h5>
+<h5 class="CertDigVal2">2021-08-15</h5>
+</div>
+<div class="col-md-6">
+<h5 class="CertDigTit2">País de vacunación / Country of Vaccination</h5>
+<h5 class="CertDigVal2">CO-Colombia</h5>
+</div>
+</div>
+</div>
+
+<img src="https://mivacuna.sispro.gov.co/MiVacuna/Images/MinSaludDigitalFoot.png" alt="" style="width:100%; padding-bottom:0px;" />
+</div>
+
+
+
+</div>
+</section>
+</div>
+
+<footer style="background-color: #004884;">
+<div style=" text-align: center;
+border-top-color: #004884;
+border-top-style: solid;
+color: #fff">
+<div style="height:10px;"></div>
+<p>
+<span style="font-weight:bold;">
+</span>
+
+</p>
+<p style="font-weight:bold;">
+Copyright © 2021 - Ministerio de Salud y Protección Social<br />
+Versión - 2021.12.9.1
+</p>
+<div style="height:10px;"></div>
+</div>
+</footer>
+</div>
+
+<script src="https://mivacuna.sispro.gov.co/MiVacuna/bundles/bootstrap?v=M4Nk6kIOwMFflsEKET0iPL9i5YBqbzMzvUOrd8gyCnw1"></script>
+
+<script src="/MiVacuna/bundles/bootstrap?v=M4Nk6kIOwMFflsEKET0iPL9i5YBqbzMzvUOrd8gyCnw1"></script>
+
+
+
+</body>
+</html>
+ 
